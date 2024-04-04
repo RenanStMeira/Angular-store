c2a219fc72fd48e0c7cd3ada286198b15af1cf5d
RenanStMeira <renan.meira25@gmail.com>
2024-04-04 18:38:26 -0300

    create: estilo header, footer e card

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -6,27 +6,15 @@ import { Product } from 'models/product.interface';
 @Component({
   selector: 'app-products',
   template: `
-  <section class="text-gray-600 body-font">
-  <div class="container px-5 py-24 mx-auto">
-    <div class="flex flex-wrap -m-4">
-
-      <app-card *ngFor="let product of products" [product]="product" (productClick)="onProductClick(product)" class="w-full p-4 lg:w1/4 md:w-1/2"></app-card>
-
-    </div>
-  </div>
-</section>
+  <section style="display: grid; grid-template-columns: repeat(auto-fill, minmax(23%, 1fr)); gap: 1rem;">
+    <app-card *ngFor="let product of products" [product]="product" (productClick)="onProductClick(product)"></app-card>
+  </section>
   `,
 })
 export class ProductsComponent implements OnInit {
   products: Product[] = [];
 
   constructor(private productService: ProductService, private router: Router) { }
-
-  // ngOnInit() {
-  //   this.productService.getProducts().subscribe(products => {
-  //     this.products = products;
-  //   });
-  // }
 
   ngOnInit() {
     this.productService.getProductsLimit5().subscribe(products => {
--- src/app/pages/products/products-page.component.ts
+++ src/app/pages/products/products-page.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-products',
   templateUrl: './products-page.component.html',
-  styleUrl: './products-page.component.css'
+
 })
 export class ProductsPageComponent {
 
